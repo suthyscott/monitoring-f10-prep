9bdb5a412e562466f117fd5bc85b613e79fab3ee
suthyscott <sutherlandscott21@gmail.com>
2021-10-25 19:54:57 -0600

    built out front end

--- server.js
+++ server.js
@@ -23,12 +23,22 @@ app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
 
-    students.push(name)
+    const index = students.findIndex(studentName=> studentName === name)
 
-    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
 
-    res.status(200).send(students)
 })
+
 
 
 app.use(rollbar.errorHandler())
